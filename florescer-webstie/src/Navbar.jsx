c629b3646841e9a1d7eaa9fe38efac5916d5d285
Ga Courel <gabrielgcourel@gmail.com>
2025-09-21 02:49:35 -0300

    feature/criação dos modais dos tópicos do navbar

--- florescer-webstie/src/Navbar.jsx
+++ florescer-webstie/src/Navbar.jsx
@@ -6,10 +6,16 @@ import logoCompleta from "./assets/logos/logo-completa.png"
 import logo from "./assets/logos/logo.png"
 import header from "./assets/fundos/header.png"
 import headerDebaixo from "./assets/fundos/header debaixo.png"
-
+import { useState } from "react"
 
 function Navbar ()
 {
+
+    //useStates para abrir e fechar os modais dos tópicos
+    const [escolaOpen, setEscolaOpen] = useState (false);
+    const [ensinoOpen, setEnsinoOpen] = useState (false);
+    const [festasOpen, setFestasOpen] = useState (false);
+
     return(
         
         <div className="t-0 md:h-44 flex items-center relative">
@@ -22,27 +28,121 @@ function Navbar ()
             {/* logo Florescer */}
             <div className="ml-4 flex flex-col justify-center items-center">
                 <img src={logo} className="h-26 hover:scale-110 duration-300 cursor-pointer" />
-                <img src={florescerJardim} className="h-13" />
+                <img src={florescerJardim} className="h-13 cursor-pointer" />
             </div>
 
             <div className="flex items-center gap-10 mx-auto">
 
                 {/* tópico escola */}
-                <div className="relative flex justify-center items-center cursor-pointer group">
-                    <img src={pincelada} className="h-16 w-60" />
-                    <div className="absolute z-10 flex justify-center items-center font-Waldorf1 text-2xl text-rosa-quase group-hover:text-white group-hover:scale-110 duration-400 transition-all ease-in-out">nossa escola</div>
+                <div 
+                    className={`relative h-20 flex justify-center items-center group/nav`}
+                    onMouseEnter={ () => setEscolaOpen (true) }
+                    onMouseLeave={ () => setEscolaOpen (false) }
+                >
+                    <img src={pincelada} className="h-16 w-60 group-hover/nav:scale-105 transtition-all duration-400 cursor-pointer" />
+                    <div className="absolute z-10 flex justify-center items-center font-Waldorf1 text-2xl text-rosa-quase group-hover/nav:text-white duration-400 transition-all ease-in-out cursor-pointer">
+                        <span> nossa escola </span>
+                        <svg 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            fill="none" 
+                            viewBox="0 0 24 24" 
+                            strokeWidth={3} // Aumenta a espessura da seta
+                            stroke="currentColor" 
+                            className={`ml-1 w-6 h-6 transition-transform duration-400 ${ escolaOpen ? "rotate-180" : "" }`}>
+                            <path strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                        </svg>
+
+                        {/* modal controlado por useState */}
+                        <div className={`absolute min-w-63 max-w-fit h-fit p-6 bg-roxo-escuro rounded-xl text-gray-300 text-lg font-Waldorf2 transition-all flex flex-col items-start gap-2 cursor-default ${ escolaOpen ? "opacity-100 translate-y-24 pointer-events-auto" : "opacity-0 translate-y-4 pointer-events-none"}`}>
+                            <div className="cursor-pointer group">
+                                <p className="transition-all duration-400 group-hover:text-white">A história da Florescer</p>
+                                <div className="h-px bg-white scale-x-0 group-hover:scale-x-100 transition-all duration-400 ease-in-out transform-gpu"></div>   
+                            </div>
+                            <div className="cursor-pointer group">
+                                <p className="transition-all duration-400 group-hover:text-white">A pedagogia Waldorf</p>
+                                <div className="h-px bg-white scale-x-0 group-hover:scale-x-100 transition-all duration-400 ease-in-out transform-gpu"></div>   
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
                 
                 {/* tópico ensino */}
-                <div className="relative flex justify-center items-center cursor-pointer group">
-                    <img src={pincelada} className="h-16 w-60" />
-                    <div className="absolute z-10 flex justify-center items-center font-Waldorf1 text-2xl text-rosa-quase group-hover:text-white group-hover:scale-110 duration-400 transition-all ease-in-out">ensino</div>
+                <div 
+                    className={`relative h-20 flex justify-center items-center group/nav`}
+                    onMouseEnter={ () => setEnsinoOpen (true) }
+                    onMouseLeave={ () => setEnsinoOpen (false) }
+                >
+                    <img src={pincelada} className="h-16 w-60 cursor-pointer group-hover/nav:scale-105 transition-all duration-400"/>
+                    <div className="absolute z-10 flex justify-center items-center font-Waldorf1 text-2xl text-rosa-quase group-hover/nav:text-white group-hover:scale-110 duration-400 transition-all ease-in-out cursor-pointer">
+                        <span>ensino</span>
+                        <svg 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            fill="none" 
+                            viewBox="0 0 24 24" 
+                            strokeWidth={3} 
+                            stroke="currentColor" 
+                            className={`ml-1 w-6 h-6 transition-transform duration-400 ${ ensinoOpen ? "rotate-180" : "" }`}>
+                            <path strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                        </svg>
+
+                        {/* modal controlado por useState */}
+                        <div className={`absolute min-w-63 max-w-fit h-fit p-6 bg-roxo-escuro rounded-xl text-gray-300 text-lg font-Waldorf2 transition-all flex flex-col items-start gap-2 cursor-default ${ ensinoOpen ? "opacity-100 translate-y-24 pointer-events-auto" : "opacity-0 translate-y-4 pointer-events-none"}`}>
+                            <div className="cursor-pointer group">
+                                <p className="transition-all duration-400 group-hover:text-white">Jardim de infância</p>
+                                <div className="h-px bg-white scale-x-0 group-hover:scale-x-100 transition-all duration-400 ease-in-out transform-gpu"></div>   
+                            </div>
+                            <div className="cursor-pointer group">
+                                <p className="transition-all duration-400 group-hover:text-white">Maternal</p>
+                                <div className="h-px bg-white scale-x-0 group-hover:scale-x-100 transition-all duration-400 ease-in-out transform-gpu"></div>   
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
 
                 {/* tópico festas */}
-                <div className="relative flex justify-center items-center cursor-pointer group duration-300">
-                    <img src={pincelada} className="h-16 w-60" />
-                    <div className="absolute z-10 flex justify-center items-center font-Waldorf1 text-2xl text-rosa-quase group-hover:text-white group-hover:scale-110 duration-400 transition-all ease-in-out">festas</div>
+                <div 
+                    className={`relative h-20 flex justify-center items-center group/nav`}
+                    onMouseEnter={ () => setFestasOpen (true) }
+                    onMouseLeave={ () => setFestasOpen (false) }
+                >
+                    <img src={pincelada} className="h-16 w-60 cursor-pointer group-hover/nav:scale-105 transition-all duration-400" />
+                    <div className="absolute z-10 flex justify-center items-center font-Waldorf1 text-2xl text-rosa-quase group-hover/nav:text-white group-hover:scale-110 duration-400 transition-all ease-in-out cursor-pointer">
+                        <span>festas</span>
+                        <svg 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            fill="none" 
+                            viewBox="0 0 24 24" 
+                            strokeWidth={3} 
+                            stroke="currentColor" 
+                            className={`ml-1 w-6 h-6 transition-transform duration-400 ${ festasOpen ? "rotate-180" : "" }`}>
+                            <path strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                        </svg>
+
+                        {/* modal controlado por useState */}
+                        <div className={`absolute min-w-63 max-w-fit h-fit p-6 bg-roxo-escuro rounded-xl text-gray-300 text-lg font-Waldorf2 transition-all flex flex-col items-start gap-2 cursor-default ${ festasOpen ? "opacity-100 translate-y-33.5 pointer-events-auto" : "opacity-0 translate-y-15 pointer-events-none"}`}>
+                            <div className="cursor-pointer group">
+                                <p className="transition-all duration-400 group-hover:text-white">Festa da lanterna</p>
+                                <div className="h-px bg-white scale-x-0 group-hover:scale-x-100 transition-all duration-400 ease-in-out transform-gpu"></div>   
+                            </div>
+                            <div className="cursor-pointer group">
+                                <p className="transition-all duration-400 group-hover:text-white">Festa da família </p>
+                                <div className="h-px bg-white scale-x-0 group-hover:scale-x-100 transition-all duration-400 ease-in-out transform-gpu"></div>   
+                            </div>
+                            <div className="cursor-pointer group">
+                                <p className="transition-all duration-400 group-hover:text-white">Bazar de natal</p>
+                                <div className="h-px bg-white scale-x-0 group-hover:scale-x-100 transition-all duration-400 ease-in-out transform-gpu"></div>   
+                            </div>
+                            <div className="cursor-pointer group">
+                                <p className="transition-all duration-400 group-hover:text-white">Festa junina</p>
+                                <div className="h-px bg-white scale-x-0 group-hover:scale-x-100 transition-all duration-400 ease-in-out transform-gpu"></div>   
+                            </div>
+                        </div>
+
+                    </div>
+               
+               
                 </div>
 
                 {/* tópico contato */}
